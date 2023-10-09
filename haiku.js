9bfa054f06836d77a4d08c97c3d850a041e6b3a6
jona <jona.wehnes@gmail.com>
2023-10-09 09:55:34 +0200

    (jona2)

--- haiku.js
+++ haiku.js
@@ -3,16 +3,16 @@
 //plak hier de haiku functions onder elkaar
 
 function Haiku1(){
-    console.log("haiku by:","AUTHOR");
+    console.log("haiku by:","Angelee Deodhar");
     console.log("Stars adrift");
     console.log("In the chill of night");
     console.log("The last dairy entry");
 }
 function Haiku2(){
-    console.log("haiku by:","AUTHOR");
-    console.log("REGEL1");
-    console.log("REGEL2");
-    console.log("REGEL3");
+    console.log("haiku by:","Katsushika Hokusai");
+    console.log("I write, erase, rewrite");
+    console.log("Erase again, and then");
+    console.log("A poppy blooms.");
 }
 function Haiku3(){
     console.log("haiku by:","AUTHOR");
