c63a8138b8dee941430a50df00b74cabb16712af
Pepijn te Winkel <37923@ma-web.nl>
2023-10-09 09:50:13 +0200

    haiku1

--- haiku.js
+++ haiku.js
@@ -2,7 +2,19 @@
 
 //plak hier de haiku functions onder elkaar
 
-function HaikuX(){
+function Haiku1(){
+    console.log("haiku by:","AUTHOR");
+    console.log("Stars adrift");
+    console.log("In the chill of night");
+    console.log("The last dairy entry");
+}
+function Haiku2(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function Haiku3(){
     console.log("haiku by:","AUTHOR");
     console.log("REGEL1");
     console.log("REGEL2");
